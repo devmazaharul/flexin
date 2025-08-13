d29979d50a18780d04b9e44771847bdd85dffa81
mazaharul islam <mazaharul>
2025-08-13 15:35:18 +0600

    feat(single product):new feature implemnt

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function page() {
   return (
     <div className='h-screen items-center w-full flex text-center'>
       <Button variant={"default"}>
+        <h1>Produc list</h1>
         <Link href={'/products'}>Products</Link>
       </Button>
     </div>
