a7ab731fd0f0967a68bc9223fda1ce804bb5e11e
mazaharul islam <mazaharul>
2025-09-01 16:00:38 +0600

    fix:env variable issue

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>this is policy page</div>
+    <div>this is policy page {process.env.NEXT_PUBLIC_AXIOM_TOKEN}</div>
   )
 }
